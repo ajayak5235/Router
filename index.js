82c7b7e0699688a7bcac89538a2337d44aee23f5
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2023-09-07 14:29:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 
 const express = require('express');
-const bodyPrser = require('body-parser');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoder({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page not found </h1>')
+})
 
 
 app.listen(3000);
@@ -19,37 +22,8 @@ app.listen(3000);
 
 
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
 
-// const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome to the home page.');
-// });
 
-// app.get('/add-product', (req, res) => {
-//     res.send(`
-//         <form action="/add-product" method="POST">
-//             <input type="text" name="productName" placeholder="Product Name">
-//             <input type="text" name="productSize" placeholder="Product Size">
-//             <button type="submit">Add Product</button>
-//         </form>
-//     `);
-// });
 
-// app.post('/add-product', (req, res) => {
-//     const productName = req.body.productName;
-//     const productSize = req.body.productSize;
-
-//     console.log('Product Name:', productName);
-//     console.log('Product Size:', productSize);
-
-//     res.send('Product added successfully.');
-// });
-
-// app.listen(3000, () => {
-//     console.log('Server is running on port 3000');
-// });
